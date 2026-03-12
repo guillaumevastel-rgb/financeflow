4b43b1513de3a34da6e0f3c229a95271e85cd4f0
guillaumevastel-rgb <guillaume.vastel@gmail.com>
2026-03-12 18:57:50 +0100

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,6 @@
+// Google Drive OAuth credentials for FinanceFlow
+window.GoogleDriveCredentials = {
+  clientId: "431880199520-thsmul2moft0m7jsbb6ttlkukv59ru0r.apps.googleusercontent.com",
+  redirectUri: window.location.origin + window.location.pathname,
+  scopes: "https://www.googleapis.com/auth/drive.file"
+};
